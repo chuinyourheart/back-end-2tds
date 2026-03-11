259cd690919df9a2be715a02177515002205f382
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-03-11 08:47:52 -0300

    Add files via upload

--- endereco.js
+++ endereco.js
@@ -1,8 +1,8 @@
-console.log("Endereço completo");
-let rua = "Rua dos lírios";
+console.log("Endereço Completo");
+let rua = "Rua dos Lirios";
 let numero = 489;
 let cidade = "Curitiba";
 
-let endereco = rua + "," + numero + "-" + cidade
+let endereco = rua + "," + numero + "-" + cidade;
 //console.log(endereco);
-console.log("O endereço correto é: " + endereco )
+console.log ("O endereço correto é:" + endereco);
