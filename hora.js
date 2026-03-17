1f54c9c3b51d108bbcdecdcdc0a970e8b4b78cc2
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-03-17 14:42:55 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //Valores iniciais do Dia
 let valorPorHora = 50;
 let horasTrabalhadas = 8;
+
+console.log("---Resumo do Expediente---");
+console.log(`Horas iniciais: ${horasTrabalhadas}h. `);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
