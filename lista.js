43ba2cd1c0f998ef7bf7c753645fca0df1019c6b
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-04-15 13:44:07 +0000

    v1

---
+++ lista.js
@@ -0,0 +1,11 @@
+const alunos = ["Juniu", "Rogerio", "Roger"];
+
+alunos.push("Juju");
+alunos.unshift("Juninho");
+console.log(alunos);
+
+console.log(alunos.indexOf("Juju"));
+console.log(alunos.indexOf("Machado"));
+
+console.log(alunos.includes("Ju"));
+console.log(alunos.includes("Jujuba"));
