5d75eebb8ac73084fd82add0a16a4e56d8e1a71b
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-05-13 13:53:51 +0000

    v1

---
+++ desafio.js
@@ -0,0 +1,21 @@
+// 1. Declarar 5 variáveis para representar o perfil de um jogador
+// Fazer uma saída de forma interpolada (`${}`)
+// "Jogador", se ele eh básico, idade, apelido, jogo e time. 
+// 2. Crie duas variáveis numéricas
+
+
+let apelido = "AngelClouds";
+let idade = 22;
+let ehBasico = true;
+let jogoFavorito = "Roblox";
+let timeAtual = "Team Red (murder's X sheriff's";
+let team = "Murder"
+
+console.log(`Perfil do Jogador`);
+console.log(`Apelido: ${apelido}`);
+console.log(`Idade: ${idade} anos`);
+console.log(`Tipo de Conta: ${ehBasico} "Básico"`);
+console.log(`Jogo Principal: ${jogoFavorito}`);
+console.log(`TimeAtual: ${timeAtual}`);
+console.log(`Team: ${team} Team Red`)
+
