80939623dcf264c40926c0e6e5f837dbe0a21271
mio <isabela.oliveira.maria@escola.pr.gov.br>
2026-05-18 13:53:08 +0000

    v2

--- desafio.js
+++ desafio.js
@@ -1,21 +1,32 @@
-// 1. Declarar 5 variáveis para representar o perfil de um jogador
-// Fazer uma saída de forma interpolada (`${}`)
+// 1. Declarar 5 variáveis para representar o perfil de um jogador.
+// Fazer uma saída de forma interpolada. (`${}`)
 // "Jogador", se ele eh básico, idade, apelido, jogo e time. 
-// 2. Crie duas variáveis numéricas
+// 2. Crie duas variáveis numérica e exibir uma multiplicação ou subtração.
+// 3. Declarar pontos 1 e 2 (fases). Calcular o total, aa diferença entre fases, nível  atual e pontos fatando para o próximo nível.
 
 
+1.
 let apelido = "AngelClouds";
-let idade = 22;
+let idade = 17;
 let ehBasico = true;
-let jogoFavorito = "Roblox";
-let timeAtual = "Team Red (murder's X sheriff's";
+let jogo = "Roblox";
+let timeAtual = "Red and Blue (murder's X sheriff's)";
 let team = "Murder"
 
 console.log(`Perfil do Jogador`);
 console.log(`Apelido: ${apelido}`);
 console.log(`Idade: ${idade} anos`);
 console.log(`Tipo de Conta: ${ehBasico} "Básico"`);
-console.log(`Jogo Principal: ${jogoFavorito}`);
+console.log(`Jogo Principal: ${jogo}`);
 console.log(`TimeAtual: ${timeAtual}`);
-console.log(`Team: ${team} Team Red`)
+console.log(`Team: ${team} Team Blue`)
 
+2.
+let danosSheriff = 45;
+let danosMurder = 20;
+let danosInocente = 55;
+
+let resultado = danosSheriff - danosMurder - danosInocente;
+
+console.log(`danosSheriff
+console.log(resultado);
